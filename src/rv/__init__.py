906ca465823be6dd8b50f8653a79f507c9efff9d
tianliang.bian@gmail.com <host@hostdeMacBook-Pro.local>
2018-08-27 19:21:44 +0800

    fix rv init

---
+++ src/rv/__init__.py
@@ -0,0 +1,15 @@
+from src.rv.bernoulli import Bernoulli
+from src.rv.beta import Beta
+from src.rv.categorical import Categorical
+from src.rv.dirichlet import Dirichlet
+from src.rv.gamma import Gamma
+#from src.rv.gaussian import Gaussian
+
+__all__ = [
+    "Bernoulli",
+    "Beta",
+    "Categorical",
+    "Dirichlet",
+    "Gamma",
+    "Gaussian"
+]
