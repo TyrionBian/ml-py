04a7697dc7eda5d28a0fb30ae076b59cb81ab1c8
TyrionBian <tianliang.bian@gmail.com>
2019-03-07 18:50:41 +0800

    add linear class

--- src/linear/__init__.py
+++ src/linear/__init__.py
@@ -1,5 +1,9 @@
 from src.linear.linear_regression import LinearRegression
+from src.linear.ridge_regression import RidgeRegression
+from src.linear.bayesian_regression import BayesianRegression
 
 __all__ = [
-    "LinearRegression"
+    "LinearRegression",
+    "RidgeRegression",
+    "BayesianRegression"
 ]
